ed663c3414802e3597e2a486fb49fcc43422e8a0
Frank <frankyoh@tmon.co.kr>
2019-06-08 17:03:40 +0900

    sass 사용법

--- 09/sass-module/src/App.js
+++ 09/sass-module/src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import styles from './App.scss';
+import classNames from 'classnames/bind';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const cx = classNames.bind(styles); //bind 기능으로 styles.을 생략할 수 있음.
+
+console.log("styles", styles);
+
+class App extends Component {
+    render() {
+        const isBlue = true;
+
+        return (
+            <div className={cx('box', {
+                blue: isBlue
+            })}>
+                <div className={cx('box-inside')}>
+
+                </div>
+
+            </div>
+        );
+    }
 }
 
 export default App;
