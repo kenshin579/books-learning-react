b88886abfe17bf61b76fc2ca913438fcabeb53c8
Frank <frankyoh@tmon.co.kr>
2019-06-08 16:51:02 +0900

    classNames 사용예

--- 09/css-module/src/App.js
+++ 09/css-module/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import styles from './App.css';
 import classNames from 'classnames/bind';
@@ -7,12 +7,18 @@ const cx = classNames.bind(styles); //bind 기능으로 styles.을 생략할 수
 
 console.log("styles", styles);
 
-function App() {
-  return (
-    <div className={cx('box', 'blue')}>
+class App extends Component {
+  render() {
+    const isBlue = true;
 
-    </div>
-  );
+    return (
+        <div className={cx('box', {
+          blue:isBlue
+        })}>
+
+        </div>
+    );
+  }
 }
 
 export default App;
