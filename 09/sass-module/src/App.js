b2b08aa3a4ecb5e8d0e046e26ca2b8cee45b48a7
Frank <frankyoh@tmon.co.kr>
2019-06-08 18:07:11 +0900

    added button components

--- 09/sass-module/src/App.js
+++ 09/sass-module/src/App.js
@@ -1,23 +1,11 @@
 import React, {Component} from 'react';
-import styles from './App.scss';
-import classNames from 'classnames/bind';
-
-const cx = classNames.bind(styles); //bind 기능으로 styles.을 생략할 수 있음.
-
-console.log("styles", styles);
+import Button from './components/Button';
 
 class App extends Component {
     render() {
-        const isBlue = true;
-
         return (
-            <div className={cx('box', {
-                blue: isBlue
-            })}>
-                <div className={cx('box-inside')}>
-
-                </div>
-
+            <div>
+                <Button>버튼</Button>
             </div>
         );
     }
