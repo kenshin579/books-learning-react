a2695ed66f47d876b83a92a53ac49284485c90f0
Frank <frankyoh@tmon.co.kr>
2019-06-08 16:47:47 +0900

    classNames bind 사용

--- 09/css-module/src/App.js
+++ 09/css-module/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import styles from './App.css';
-import classNames from 'classnames';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles); //bind 기능으로 styles.을 생략할 수 있음.
 
 console.log("styles", styles);
 
 function App() {
   return (
-    <div className={classNames(styles.box,styles.blue)}>
+    <div className={cx('box', 'blue')}>
 
     </div>
   );
