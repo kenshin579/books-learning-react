86c79cda820642dcbe70bf3617cbd4d6e478b2d8
Frank <frankyoh@tmon.co.kr>
2019-06-08 16:46:14 +0900

    classNames 사용

--- 09/css-module/src/App.js
+++ 09/css-module/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import styles from './App.css';
+import classNames from 'classnames';
 
 console.log("styles", styles);
 
 function App() {
   return (
-    <div className={[styles.box, styles.blue].join(' ')}>
+    <div className={classNames(styles.box,styles.blue)}>
 
     </div>
   );
