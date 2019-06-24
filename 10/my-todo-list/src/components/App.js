64357dee853e6bcce3ce964567eb9f3eb64ccf88
Frank <frankyoh@tmon.co.kr>
2019-06-24 23:08:35 +0900

    10.3.1 텍스트 입력 관련 상태 관리

--- 10/my-todo-list/src/components/App.js
+++ 10/my-todo-list/src/components/App.js
@@ -4,10 +4,25 @@ import TodoInput from "./TodoInput";
 import TodoList from "./TodoList";
 
 class App extends Component {
+    state = {
+        input: '' //input 값
+    };
+
+    handleChange = (e) => {
+        const {value} = e.target;
+        this.setState({
+            input: value
+        });
+    };
+
     render() {
+        const {input} = this.state; //this.state을 하지 않아도 됨
+        const {handleChange} = this;
+
         return (
             <PageTemplate>
-                <TodoInput/>
+                {/*<TodoInput onChange={this.handleChange()} value={this.state.input}/>*/}
+                <TodoInput onChange={handleChange} value={input}/>
                 <TodoList/>
             </PageTemplate>
         );
