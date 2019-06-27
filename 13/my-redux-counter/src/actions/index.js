80456fddd1137baa011d999fa8354d71cc23bd26
Frank <frankyoh@tmon.co.kr>
2019-06-28 00:06:39 +0900

    스터디중

---
+++ 13/my-redux-counter/src/actions/index.js
@@ -0,0 +1,32 @@
+/*
+    action 객체를 만드는 액션 생성 함수들을 선언합니다(action creators).
+    여기서 () => ({})은 function() { return { } }와 동일한 의미입니다.
+*/
+
+import * as types from './ActionTypes';
+
+//이건 객체를 생성해서 반환하는 함수를 정의함
+export const create = (color) => ({
+    type: types.CREATE,
+    color
+});
+
+export const remove = () => ({
+    type: types.REMOVE
+});
+
+export const increment = (index) => ({
+    type: types.INCREMENT,
+    index
+});
+
+export const decrement = (index) => ({
+    type: types.DECREMENT,
+    index
+});
+
+export const setColor = ({index, color}) => ({
+    type: types.SET_COLOR,
+    index,
+    color
+});
