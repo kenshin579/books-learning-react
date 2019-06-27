4712583a7aa282c49dbec90c1ca1e27d5eb4f86d
Frank <frankyoh@tmon.co.kr>
2019-06-27 23:58:05 +0900

    13.1.4 Counter 컴포넌트 생성

---
+++ 13/my-redux-counter/src/components/Counter.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Counter.css';
+
+const Counter = ({number, color, onIncrement, onDecrement, onSetColor}) => {
+    return (
+        <div className="Counter"
+             onClick={onIncrement}
+             onContextMenu={(e) => {
+                 e.preventDefault();
+                 onDecrement();
+             }}
+             onDoubleClick={onSetColor}
+             style={{
+                 backgroundColor: color
+             }}>
+            {number}
+        </div>
+    );
+};
+
+//기본 props 타입 지정
+Counter.propTypes = {
+    number: PropTypes.number,
+    color: PropTypes.string,
+    onIncrement: PropTypes.func,
+    onDecrement: PropTypes.func,
+    onSetColor: PropTypes.func
+};
+
+//props의 기본값 설정
+Counter.defaultProps = {
+    number: 0,
+    color: 'black',
+    onIncrement: () => console.warn('onIncrement not defined'),
+    onDecrement: () => console.warn('onDecrement not defined'),
+    onSetColor: () => console.warn('onSetColor not defined')
+};
+
+export default Counter;
