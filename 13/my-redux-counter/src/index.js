c0af2055aadc3dbe059ab9572b7d5e506013344e
Frank <frankyoh@tmon.co.kr>
2019-06-28 22:39:37 +0900

    13.1.8 Provider 컴포넌트로 리액트 앱에 store 연동

--- 13/my-redux-counter/src/index.js
+++ 13/my-redux-counter/src/index.js
@@ -3,15 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
-
 // Redux 관련 불러오기
-import { createStore } from 'redux';
+import {createStore} from 'redux';
 import reducers from './reducers';
+import {Provider} from 'react-redux';
 
 // 스토어 생성
 const store = createStore(reducers);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
