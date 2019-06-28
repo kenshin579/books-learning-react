ca23e9750f0fa9b2499f40f36c63d1114026e6f0
Frank <frankyoh@tmon.co.kr>
2019-06-28 22:37:22 +0900

    13.1.7 스토어 생성

--- 13/my-redux-counter/src/index.js
+++ 13/my-redux-counter/src/index.js
@@ -4,6 +4,13 @@ import './index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 
+// Redux 관련 불러오기
+import { createStore } from 'redux';
+import reducers from './reducers';
+
+// 스토어 생성
+const store = createStore(reducers);
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
