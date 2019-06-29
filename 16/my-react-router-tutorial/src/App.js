234942cb0cdf52474cf6133384d1b1cbaeb4a7ef
Frank <frankyoh@tmon.co.kr>
2019-06-29 15:33:55 +0900

    16.4.1 Link 컴포넌트

--- 16/my-react-router-tutorial/src/App.js
+++ 16/my-react-router-tutorial/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {Route} from 'react-router-dom';
 import {About, Home} from 'pages';
+import Menu from "./components/Menu";
 
 const App = () => {
     return (
         <div>
+            <Menu/>
             <Route exact path="/" component={Home}/>
             <Route path="/about/:name?" component={About}/>
         </div>
