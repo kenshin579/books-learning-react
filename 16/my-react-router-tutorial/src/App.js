5db690ec858f310218c1f341c20c6685f2111531
Frank <frankyoh@tmon.co.kr>
2019-06-29 15:02:34 +0900

    16.3.2 router 세팅 for home and about

--- 16/my-react-router-tutorial/src/App.js
+++ 16/my-react-router-tutorial/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import {Route} from 'react-router-dom';
+import {About, Home} from 'pages';
 
 const App = () => {
     return (
         <div>
-            리액트 라우터를 배워봅시다
+            <Route exact path="/" component={Home}/>
+            <Route path="/about" component={About}/>
         </div>
-    )
+    );
 };
 
 export default App;
