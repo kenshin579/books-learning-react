83ff94f3729c8354d6195118af67b1bdf3b327fa
frankyoh <frankyoh@tmon.co.kr>
2019-08-13 23:35:34 +0900

    15.2.1 redux-thunk 적용

--- 15/my-redux-starter-kit/src/store.js
+++ 15/my-redux-starter-kit/src/store.js
@@ -2,12 +2,13 @@ import { createStore, applyMiddleware } from 'redux';
 import modules from './modules';
 
 import { createLogger } from 'redux-logger';
+import ReduxThunk from 'redux-thunk';
 
 /* 로그 미들웨어를 생성할 때 설정을 커스터마이징할 수 있습니다.
    https://github.com/evgenyrodionov/redux-logger#options
 */
 const logger = createLogger();
 
-const store = createStore(modules, applyMiddleware(logger));
+const store = createStore(modules, applyMiddleware(logger, ReduxThunk));
 
 export default store;
