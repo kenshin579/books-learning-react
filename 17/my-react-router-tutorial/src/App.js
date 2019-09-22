a422f41a57e8b7a9b0c044a50a1118e932fd520e
frankyoh <frankyoh@tmon.co.kr>
2019-09-22 20:52:26 +0900

    17 finshed studying

--- 17/my-react-router-tutorial/src/App.js
+++ 17/my-react-router-tutorial/src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import {Route} from 'react-router-dom';
-import {About, Home, Posts} from 'pages';
+// import {About, Home, Posts} from 'pages';
+import {About, Home, Posts} from 'pages/index.async.js';
 import Menu from "./components/Menu";
-import AsyncSplitMe from 'components/AsyncSplitMe';
+// import AsyncSplitMe from 'components/AsyncSplitMe';
 
 const App = () => {
     return (
         <div>
             <Menu/>
-            <AsyncSplitMe/>
+            {/*<AsyncSplitMe/>*/}
             <Route exact path="/" component={Home}/>
             <Route path="/about/:name?" component={About}/>
             <Route path="/posts" component={Posts}/>
