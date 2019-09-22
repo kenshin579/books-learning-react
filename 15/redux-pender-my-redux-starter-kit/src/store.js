919e505703c140143cc004f3abde33295915b1bd
frankyoh <frankyoh@tmon.co.kr>
2019-09-22 15:07:51 +0900

    redux-pender 추가

---
+++ 15/redux-pender-my-redux-starter-kit/src/store.js
@@ -0,0 +1,13 @@
+import {applyMiddleware, createStore} from 'redux';
+import modules from './modules';
+import {createLogger} from 'redux-logger';
+import promiseMiddleware from 'redux-promise-middleware';
+
+const logger = createLogger();
+const pm = promiseMiddleware({
+    promiseTypeSuffixes: ['PENDING', 'SUCCESS', 'FAILURE']
+});
+
+const store = createStore(modules, applyMiddleware(logger, pm));
+
+export default store;
