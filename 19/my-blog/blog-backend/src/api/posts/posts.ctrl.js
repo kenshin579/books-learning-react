dcfe711ddd82c5138cdf18577cac4e9ac047d939
frankyoh <frankyoh@tmon.co.kr>
2019-09-23 22:47:19 +0900

    19 스터디 완료

--- 19/my-blog/blog-backend/src/api/posts/posts.ctrl.js
+++ 19/my-blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -1,6 +1,38 @@
 const Post = require('models/post');
+const Joi = require('joi');
+
+const {ObjectId} = require('mongoose').Types;
+
+exports.checkObjectId = (ctx, next) => {
+    const {id} = ctx.params;
+
+    // 검증 실패
+    if (!ObjectId.isValid(id)) {
+        ctx.status = 400; // 400 Bad Request
+        return null;
+    }
+
+    return next(); // next를 리턴해주어야 ctx.body가 제대로 설정됩니다.
+};
 
 exports.write = async (ctx) => {
+    // 객체가 지닌 값들을 검증합니다.
+    const schema = Joi.object().keys({
+        title: Joi.string().required(), // 뒤에 required를 붙여주면 필수 항목이라는 의미
+        body: Joi.string().required(),
+        tags: Joi.array().items(Joi.string()).required() // 문자열 배열
+    });
+
+    // 첫 번째 파라미터는 검증할 객체, 두 번째는 스키마
+    const result = Joi.validate(ctx.request.body, schema);
+
+    // 오류 발생 시 오류 내용 응답
+    if (result.error) {
+        ctx.status = 400;
+        ctx.body = result.error;
+        return;
+    }
+
     const {title, body, tags} = ctx.request.body;
 
     const post = new Post({
@@ -16,11 +48,35 @@ exports.write = async (ctx) => {
 };
 
 exports.list = async (ctx) => {
+    // page가 주어지지 않았다면 1로 간주
+    // query는 문자열 형태로 받아오므로 숫자로 변환
+    const page = parseInt(ctx.query.page || 1, 10);
+
+    // 잘못된 페이지가 주어졌다면 오류
+    if (page < 1) {
+        ctx.status = 400;
+        return;
+    }
+
     try {
-        const posts = await Post.find().exec();
-        ctx.body = posts;
+        const posts = await Post.find()
+            .sort({_id: -1})
+            .limit(10)
+            .skip((page - 1) * 10)
+            .lean()
+            .exec();
+        const postCount = await Post.countDocuments().exec();
+        const limitBodyLength = post => ({
+            ...post,
+            body: post.body.length < 200 ? post.body : `${post.body.slice(0, 200)}...`
+        });
+
+        ctx.body = posts.map(limitBodyLength);
+        // 마지막 페이지 알려주기
+        // ctx.set은 response header를 설정해줍니다.
+        ctx.set('Last-Page', Math.ceil(postCount / 10));
     } catch (e) {
-        ctx.throw(3, 500);
+        ctx.throw(500, e);
     }
 };
 
