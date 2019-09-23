f4e9145066d70966fbab387a7ed8189f7d11bef1
frankyoh <frankyoh@tmon.co.kr>
2019-09-23 22:18:17 +0900

    19.8 기본 API 완료

---
+++ 19/my-blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -0,0 +1,65 @@
+const Post = require('models/post');
+
+exports.write = async (ctx) => {
+    const {title, body, tags} = ctx.request.body;
+
+    const post = new Post({
+        title, body, tags
+    });
+
+    try {
+        await post.save();
+        ctx.body = post;
+    } catch (e) {
+        ctx.throw(e, 500);
+    }
+};
+
+exports.list = async (ctx) => {
+    try {
+        const posts = await Post.find().exec();
+        ctx.body = posts;
+    } catch (e) {
+        ctx.throw(3, 500);
+    }
+};
+
+exports.read = async (ctx) => {
+    const {id} = ctx.params;
+    try {
+        const post = await Post.findById(id).exec();
+        if (!post) {
+            ctx.status = 404;
+            return;
+        }
+        ctx.body = post;
+    } catch (e) {
+        ctx.throw(e, 500);
+    }
+};
+
+exports.remove = async (ctx) => {
+    const {id} = ctx.params;
+    try {
+        await Post.findByIdAndRemove(id).exec();
+        ctx.status = 204;
+    } catch (e) {
+        ctx.throw(e, 500);
+    }
+};
+
+exports.update = async (ctx) => {
+    const {id} = ctx.params;
+    try {
+        const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
+            new: true
+        }).exec();
+        if (!post) {
+            ctx.status = 404;
+            return;
+        }
+        ctx.body = post;
+    } catch (e) {
+        ctx.throw(e, 500);
+    }
+};
