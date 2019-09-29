78a3e960b8a5511d1ce25d535246e32bcc3034ef
frankyoh <frankyoh@tmon.co.kr>
2019-09-29 14:11:28 +0900

    201.5.3 라우트 지정완료

--- 20/my-blog/blog-frontend/src/components/App.js
+++ 20/my-blog/blog-frontend/src/components/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import {Route, Switch} from 'react-router-dom';
+import {EditorPage, ListPage, NotFoundPage, PostPage} from 'pages';
 
 const App = () => {
     return (
         <div>
-            BlogApp
+            <Switch>
+                <Route exact path="/" component={ListPage}/>
+                <Route path="/page/:page" component={ListPage}/>
+                <Route path="/tag/:tag/:page?" component={ListPage}/>
+                <Route path="/post/:id" component={PostPage}/>
+                <Route path="/editor" component={EditorPage}/>
+                <Route component={NotFoundPage}/>
+            </Switch>
         </div>
     );
 };
