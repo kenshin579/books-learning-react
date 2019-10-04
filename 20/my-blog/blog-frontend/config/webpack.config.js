7aeb5d6eaff3770ed31ab8d2d4d3ea5b3e1d3d0b
frankyoh <frankyoh@tmon.co.kr>
2019-10-04 15:36:59 +0900

    20.2.2 글로벌 스타일 및 스타일 유틸 설정완료 - 잘 안됨 ;;

--- 20/my-blog/blog-frontend/config/webpack.config.js
+++ 20/my-blog/blog-frontend/config/webpack.config.js
@@ -457,7 +457,13 @@ module.exports = function(webpackEnv) {
                   sourceMap: isEnvProduction && shouldUseSourceMap,
                 },
                 'sass-loader'
-              ),
+              ).concat({
+                loader: require.resolve('sass-loader'),
+                options: {
+                  includePaths:[paths.appSrc + '/styles'],
+                  data : `@import 'utils';`
+                }
+              }),
               // Don't consider CSS imports dead code even if the
               // containing package claims to have no side effects.
               // Remove this when webpack adds a warning or an error for this.
