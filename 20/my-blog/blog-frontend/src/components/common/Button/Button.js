552bb68d85e4064694845674e062c659fab19a9c
frankyoh <frankyoh@tmon.co.kr>
2019-10-04 20:14:02 +0900

    20.3.2 PostList 컴포넌트 생성

--- 20/my-blog/blog-frontend/src/components/common/Button/Button.js
+++ 20/my-blog/blog-frontend/src/components/common/Button/Button.js
@@ -13,8 +13,6 @@ const Button = ({children, to, onClick, disabled, theme = 'default'}) => {
     // to 값이 존재하면 Link를 사용하고, 그렇지 않으면 div를 사용합니다.
     // 비활성화되어 있는 버튼인 경우에도 div가 사용됩니다.
     const Element = (to && !disabled) ? Link : Div;
-    console.log('children:', children, 'to:', to, 'disabled:', disabled);
-    console.log('Element', Element);
 
     // 비활성화되면 onClick은 실행되지 않습니다
     // disabled 값이 true가 되면 className에 disabled가 추가됩니다.
