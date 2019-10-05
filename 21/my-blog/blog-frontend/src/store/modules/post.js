37d07d05108f32a3ed989268d28214652b476a0a
frankyoh <frankyoh@tmon.co.kr>
2019-10-05 21:51:38 +0900

    chap20 소스 복사함

---
+++ 21/my-blog/blog-frontend/src/store/modules/post.js
@@ -0,0 +1,13 @@
+import {handleActions} from 'redux-actions';
+
+import {Map} from 'immutable';
+
+// action types
+
+// action creators
+
+// initial state
+const initialState = Map({});
+
+// reducer
+export default handleActions({}, initialState);
