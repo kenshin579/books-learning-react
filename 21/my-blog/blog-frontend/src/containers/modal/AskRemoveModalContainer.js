b9ccc5319092a628ecc003b8cb26d7f0deee09b5
frankyoh <frankyoh@tmon.co.kr>
2019-10-14 22:22:49 +0900

    21.4.3.4 삭제 모달 버튼 기능 구현

--- 21/my-blog/blog-frontend/src/containers/modal/AskRemoveModalContainer.js
+++ 21/my-blog/blog-frontend/src/containers/modal/AskRemoveModalContainer.js
@@ -8,9 +8,22 @@ import {withRouter} from 'react-router-dom';
 
 class AskRemoveModalContainer extends Component {
     handleCancel = () => {
+        const {BaseActions} = this.props;
+        BaseActions.hideModal('remove');
     };
 
     handleConfirm = async () => {
+        const {BaseActions, PostActions, history, match} = this.props;
+        const {id} = match.params;
+
+        try {
+            // 포스트 삭제 후, 모달 닫고 홈페이지로 이동
+            await PostActions.removePost(id);
+            BaseActions.hideModal('remove');
+            history.push('/');
+        } catch (e) {
+            console.log(e);
+        }
     };
 
     render() {
